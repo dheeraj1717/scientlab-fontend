ac3fb08a76af5394d7b686c3eed00638166b248a
charan <charan.ku1605@gmail.com>
2023-07-16 09:49:17 +0530

    added roles to the sideBar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChartBar } from '@fortawesome/free-solid-svg-icons';
 import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from 'react-redux';
 import './Sidebar.css';
 
 function Sidebar() {
@@ -11,12 +12,24 @@ function Sidebar() {
   const handleItemClick = (item) => {
     setActiveItem(item);
   };
+  const user = useSelector((state) => state.auth.user);
 
-  const sidebarItems = [
+
+  const adminSidebarItems = [
     { id: 1, label: 'Summary', icon: faChartBar, nav: '/dashboard' },
     { id: 2, label: 'Device Metrics', icon: faChartLine, nav: '/device' },
     { id: 3, label: 'Environment Metrics', icon: faChartLine, nav: '/environment' }
   ];
+
+  const  customerSidebarItems = [
+    { id: 1, label: 'Summary', icon: faChartBar, nav: '/dashboard' },
+    ];
+
+
+  const role = `${user.role}`;
+
+  const sidebarItems = (role === "admin"? adminSidebarItems: customerSidebarItems)
+
 
   return (
     <div className="sidebar">
@@ -26,7 +39,10 @@ function Sidebar() {
           to={item.nav}
           className={`sidebar-item ${activeItem.label === item.label ? 'active' : ''}`}
           onClick={() => handleItemClick(item)}
-        ><FontAwesomeIcon icon={item.icon} /> {item.label}</Link>
+        >
+          <FontAwesomeIcon icon={item.icon} /> 
+          {item.label}
+           </Link>
       )}
     </div>
   );
