528a0eba704fceb341e71b1e5e4244018918abf0
Dheeraj Jangid <dheerajjangid1717@gmail.com>
2023-08-04 11:49:24 +0530

    Map Changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,196 +1,272 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Map from './Map';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-datetime-picker/dist/DateTimePicker.css';
-import 'react-circular-progressbar/dist/styles.css';
-
-import { clearStats, clearMetrics, getDevices, getStats } from '../redux/actions/deviceActions';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import MouseoverMap from "./Map";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-datetime-picker/dist/DateTimePicker.css";
+import "react-circular-progressbar/dist/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX, faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  clearStats,
+  clearMetrics,
+  getDevices,
+  getStats,
+} from "../redux/actions/deviceActions";
 
 function Dashboard() {
   const dispatch = useDispatch();
   const [openCharts, setOpenCharts] = useState(false);
   const [device, setDevice] = useState(null);
-  const devices = useSelector(state => state.devices.devices);
-  const stats = useSelector(state => state.devices.stats);
+  const devices = useSelector((state) => state.devices.devices);
+  const stats = useSelector((state) => state.devices.stats);
   const user = useSelector((state) => state.auth.user);
 
   useEffect(() => {
-    dispatch(getDevices(user.accessToken))
-  }, [])
+    dispatch(getDevices(user.accessToken));
+  }, []);
 
   const handleMarkerClick = (point) => {
     console.log(`selected location: ${point.location}`);
-    dispatch(clearMetrics())
-    dispatch(getStats(point.name, point.location, user.accessToken))
+    dispatch(clearMetrics());
+    dispatch(getStats(point.name, point.location, user.accessToken));
     setDevice(point);
     setOpenCharts(true);
   };
 
   const removeCharts = () => {
-    dispatch(clearStats())
-    dispatch(clearMetrics())
-    setDevice({})
-    setOpenCharts(false)
+    dispatch(clearStats());
+    dispatch(clearMetrics());
+    setDevice({});
+    setOpenCharts(false);
   };
 
   return (
     <div>
-      {openCharts ? <div>
-        <Button variant="outline-danger" className="close-button" onClick={removeCharts}>
-          X
-        </Button>
-        <p style={{
-          margin: "auto",
-          width: "50%",
-          padding: "10px",
-          display: "inline-block",
-          color: "#0a58ca"
-        }}>Device: <b>{device.name}</b>     |     Location: <b>{device.location}</b>      |     Region: <b>{device.region}</b></p>
-        <hr />
-        <Container>
-          <Row>
-            <Col>
-              <Row>
-                <h6 style={{ textAlign: "center", color: "#0a58ca" }}>Device Metrics</h6>
-                <Col>
-                  <div style={{ width: '100px', height: '100px' }}>
-                    Temperature_C
-                    <CircularProgressbar
-                      value={stats.temperatureC}
-                      text={stats && stats.temperatureC ? `${stats.temperatureC}%` : '0%'}
-                      strokeWidth={12}
-                      styles={buildStyles({
-                        rotation: 0.7,
-                        strokeLinecap: 'butt',
-                        pathTransitionDuration: 0.5,
-                        pathColor: parseInt(stats.temperatureC) < 60 ? `#3B98D6` : `red`,
-                        textColor: '#333333',
-                        trailColor: '#f8f8f8',
-                        backgroundColor: '#e4e4e4',
-                      })}
-                    />
-                  </div>
-                </Col>
-                <Col>
-                  <div style={{ width: '110px', height: '100px' }}>
-                    CPU Usage
-                    <CircularProgressbar
-                      value={stats.cpuUsage}
-                      text={stats && stats.cpuUsage ? `${stats.cpuUsage}%` : '0%'}
-                      strokeWidth={12}
-                      styles={buildStyles({
-                        rotation: 0.7,
-                        strokeLinecap: 'butt',
-                        pathTransitionDuration: 0.5,
-                        pathColor: parseInt(stats.cpuUsage) < 60 ? `#3B98D6` : `red`,
-                        textColor: '#333333',
-                        trailColor: '#f8f8f8',
-                        backgroundColor: '#e4e4e4',
-                      })}
-                    />
-                  </div>
-                </Col>
-                <Col>
-                  <div style={{ width: '110px', height: '100px' }}>
-                    Memory Usage
-                    <CircularProgressbar
-                      value={stats.memoryUsage}
-                      text={stats && stats.memoryUsage ? `${stats.memoryUsage}%` : '0%'}
-                      strokeWidth={12}
-                      styles={buildStyles({
-                        rotation: 0.7,
-                        strokeLinecap: 'butt',
-                        pathTransitionDuration: 0.5,
-                        pathColor: parseInt(stats.memoryUsage) < 60 ? `#3B98D6` : `red`,
-                        textColor: '#333333',
-                        trailColor: '#f8f8f8',
-                        backgroundColor: '#e4e4e4',
-                      })}
-                    />
-                  </div>
-                </Col></Row>
-              <div style={{ paddingTop: "40px" }}></div>
-              <hr />
-              <Row>
-                <h6 style={{ textAlign: "center", color: "#0a58ca" }}>Environment Metrics</h6>
-                <Col>
-                  <div style={{ width: '110px', height: '100px' }}>
-                    Humidity
-                    <CircularProgressbar
-                      value={stats.roomHumidity}
-                      text={stats && stats.roomHumidity ? `${stats.roomHumidity}%` : '0%'}
-                      strokeWidth={12}
-                      styles={buildStyles({
-                        rotation: 0.7,
-                        strokeLinecap: 'butt',
-                        pathTransitionDuration: 0.5,
-                        pathColor: parseInt(stats.roomHumidity) < 60 ? `#3B98D6` : `red`,
-                        textColor: '#333333',
-                        trailColor: '#f8f8f8',
-                        backgroundColor: '#e4e4e4',
-                      })}
-                    />
-                  </div>
-                </Col>                <Col>
-                  <div style={{ width: '110px', height: '100px' }}>
-                    Temperature_C
-                    <CircularProgressbar
-                      value={stats.roomTemperatureC}
-                      text={stats && stats.roomTemperatureC ? `${stats.roomTemperatureC}%` : '0%'}
-                      strokeWidth={12}
-                      styles={buildStyles({
-                        rotation: 0.7,
-                        strokeLinecap: 'butt',
-                        pathTransitionDuration: 0.5,
-                        pathColor: parseInt(stats.roomTemperatureC) < 60 ? `#3B98D6` : `red`,
-                        textColor: '#333333',
-                        trailColor: '#f8f8f8',
-                        backgroundColor: '#e4e4e4',
-                      })}
-                    />
-                  </div>
-                </Col>
-                <Col>
-                  <div style={{ width: '110px', height: '100px' }}>
-                    Temperature_F
-                    <CircularProgressbar
-                      value={stats.roomTemperatureF}
-                      text={stats && stats.roomTemperatureF ? `${stats.roomTemperatureF}%` : '0%'}
-                      strokeWidth={12}
-                      styles={buildStyles({
-                        rotation: 0.7,
-                        strokeLinecap: 'butt',
-                        pathTransitionDuration: 0.5,
-                        pathColor: parseInt(stats.roomTemperatureF) < 60 ? `#3B98D6` : `red`,
-                        textColor: '#333333',
-                        trailColor: '#f8f8f8',
-                        backgroundColor: '#e4e4e4',
-                      })}
-                    />
-                  </div>
-                </Col>
-              </Row>
-            </Col>
-            <Col><Map dataPoints={devices} handleMarkerClick={handleMarkerClick} st={{
-              height: '300px',
-              width: '100%',
-              zoom: 1.5,
-              center: [40, 60],
-              dataPoint: device
-            }} /></Col>
-          </Row>
-        </Container>
-      </div> : <div><span>Click on the <b>DataPoints</b> to view the Device Metrics</span>
-        <Map dataPoints={devices} handleMarkerClick={handleMarkerClick} st={{
-          height: '300px',
-          width: '100%',
-          zoom: 2,
-          center: [30, 70],
-          dataPoint: device
-        }} /></div>}
-
+      {openCharts ? (
+        <div>
+          <Button
+            variant="outline-danger"
+            className="close-button "
+            onClick={removeCharts}
+          >
+            <FontAwesomeIcon icon={faX} />
+          </Button>
+          <p
+            style={{
+              margin: "auto",
+              width: "50%",
+              padding: "10px",
+              display: "inline-block",
+              color: "#0a58ca",
+            }}
+          >
+            Device: <b>{device.name}</b> | Location: <b>{device.location}</b> |
+            Region: <b>{device.region}</b>
+          </p>
+          <Container>
+            <Row>
+              <Col>
+                <Row>
+                  <h6 style={{ textAlign: "center", color: "#0a58ca" }}>
+                    Device Metrics
+                  </h6>
+                  <Col>
+                    <div style={{ width: "100px", height: "100px" }}>
+                      Temperature_C
+                      <CircularProgressbar
+                        value={stats.temperatureC}
+                        text={
+                          stats && stats.temperatureC
+                            ? `${stats.temperatureC}%`
+                            : "0%"
+                        }
+                        strokeWidth={12}
+                        styles={buildStyles({
+                          rotation: 0.7,
+                          strokeLinecap: "butt",
+                          pathTransitionDuration: 0.5,
+                          pathColor:
+                            parseInt(stats.temperatureC) < 60
+                              ? `#3B98D6`
+                              : `red`,
+                          textColor: "#333333",
+                          trailColor: "#f8f8f8",
+                          backgroundColor: "#e4e4e4",
+                        })}
+                      />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div style={{ width: "110px", height: "100px" }}>
+                      CPU Usage
+                      <CircularProgressbar
+                        value={stats.cpuUsage}
+                        text={
+                          stats && stats.cpuUsage ? `${stats.cpuUsage}%` : "0%"
+                        }
+                        strokeWidth={12}
+                        styles={buildStyles({
+                          rotation: 0.7,
+                          strokeLinecap: "butt",
+                          pathTransitionDuration: 0.5,
+                          pathColor:
+                            parseInt(stats.cpuUsage) < 60 ? `#3B98D6` : `red`,
+                          textColor: "#333333",
+                          trailColor: "#f8f8f8",
+                          backgroundColor: "#e4e4e4",
+                        })}
+                      />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div style={{ width: "110px", height: "100px" }}>
+                      Memory Usage
+                      <CircularProgressbar
+                        value={stats.memoryUsage}
+                        text={
+                          stats && stats.memoryUsage
+                            ? `${stats.memoryUsage}%`
+                            : "0%"
+                        }
+                        strokeWidth={12}
+                        styles={buildStyles({
+                          rotation: 0.7,
+                          strokeLinecap: "butt",
+                          pathTransitionDuration: 0.5,
+                          pathColor:
+                            parseInt(stats.memoryUsage) < 60
+                              ? `#3B98D6`
+                              : `red`,
+                          textColor: "#333333",
+                          trailColor: "#f8f8f8",
+                          backgroundColor: "#e4e4e4",
+                        })}
+                      />
+                    </div>
+                  </Col>
+                </Row>
+                <div style={{ paddingTop: "40px" }}></div>
+                <hr />
+                <Row>
+                  <h6 style={{ textAlign: "center", color: "#0a58ca" }}>
+                    Environment Metrics
+                  </h6>
+                  <Col>
+                    <div style={{ width: "110px", height: "100px" }}>
+                      Humidity
+                      <CircularProgressbar
+                        value={stats.roomHumidity}
+                        text={
+                          stats && stats.roomHumidity
+                            ? `${stats.roomHumidity}%`
+                            : "0%"
+                        }
+                        strokeWidth={12}
+                        styles={buildStyles({
+                          rotation: 0.7,
+                          strokeLinecap: "butt",
+                          pathTransitionDuration: 0.5,
+                          pathColor:
+                            parseInt(stats.roomHumidity) < 60
+                              ? `#3B98D6`
+                              : `red`,
+                          textColor: "#333333",
+                          trailColor: "#f8f8f8",
+                          backgroundColor: "#e4e4e4",
+                        })}
+                      />
+                    </div>
+                  </Col>{" "}
+                  <Col>
+                    <div style={{ width: "110px", height: "100px" }}>
+                      Temperature_C
+                      <CircularProgressbar
+                        value={stats.roomTemperatureC}
+                        text={
+                          stats && stats.roomTemperatureC
+                            ? `${stats.roomTemperatureC}%`
+                            : "0%"
+                        }
+                        strokeWidth={12}
+                        styles={buildStyles({
+                          rotation: 0.7,
+                          strokeLinecap: "butt",
+                          pathTransitionDuration: 0.5,
+                          pathColor:
+                            parseInt(stats.roomTemperatureC) < 60
+                              ? `#3B98D6`
+                              : `red`,
+                          textColor: "#333333",
+                          trailColor: "#f8f8f8",
+                          backgroundColor: "#e4e4e4",
+                        })}
+                      />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div style={{ width: "110px", height: "100px" }}>
+                      Temperature_F
+                      <CircularProgressbar
+                        value={stats.roomTemperatureF}
+                        text={
+                          stats && stats.roomTemperatureF
+                            ? `${stats.roomTemperatureF}%`
+                            : "0%"
+                        }
+                        strokeWidth={12}
+                        styles={buildStyles({
+                          rotation: 0.7,
+                          strokeLinecap: "butt",
+                          pathTransitionDuration: 0.5,
+                          pathColor:
+                            parseInt(stats.roomTemperatureF) < 60
+                              ? `#3B98D6`
+                              : `red`,
+                          textColor: "#333333",
+                          trailColor: "#f8f8f8",
+                          backgroundColor: "#e4e4e4",
+                        })}
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                {" "}
+                <MouseoverMap
+                  locations={devices}
+                  handleMarkerClick={handleMarkerClick}
+                />
+                <div
+                  style={{
+                    color: "red",
+                    textAlign: "center",
+                    marginTop: "20px",
+                  }}
+                >
+                  Hover on Map and scroll to zoom{" "}
+                  <FontAwesomeIcon icon={faSearchPlus} />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      ) : (
+        <div>
+          <span>
+            Click on the <b>DataPoints</b> to view the Device Metrics
+          </span>
+          <MouseoverMap
+            locations={devices}
+            handleMarkerClick={handleMarkerClick}
+          />
+          <div style={{ color: "red", textAlign: "center", marginTop: "20px" }}>
+            Hover on Map and scroll to zoom{" "}
+            <FontAwesomeIcon icon={faSearchPlus} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,53 +1,69 @@
-import React, { useState } from 'react';
-import { ComposableMap, Geographies, Geography, Marker, ZoomableGroup } from 'react-simple-maps';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarker } from "@fortawesome/free-solid-svg-icons";
+import { renderToString } from "react-dom/server"; // Import ReactDOMServer
 
-function Map({ dataPoints, handleMarkerClick, st }) {
-    const [hoveredMarker, setHoveredMarker] = useState(null);
-    const handleMarkerHover = (marker) => {
-        setHoveredMarker(marker);
-    };
+function MouseoverMap({ locations, handleMarkerClick }) {
+  const [hoveredMarker, setHoveredMarker] = useState(null);
 
-    const handleMarkerLeave = () => {
-        setHoveredMarker(null);
-    };
-    const geoUrl =
-        "/features.json";
-    return (
-        <div>
-            <ComposableMap
-                projection="geoMercator"
-                projectionConfig={{ scale: 100 }}
-                style={{ height: st.height, width: st.width }}
-            >
-                <ZoomableGroup center={st.center} zoom={st.zoom}>
-                    <Geographies geography={geoUrl}>
-                        {({ geographies }) =>
-                            geographies.map((geo) => (
-                                <Geography key={geo.rsmKey} geography={geo} fill="#EAEAEC" stroke="#D6D6DA" />
-                            ))
-                        }
-                    </Geographies>
-                    {dataPoints.map((dataPoint, index) => (
-                        <Marker
-                            key={index}
-                            coordinates={[dataPoint.longitude, dataPoint.latitude]}
-                            onClick={() => handleMarkerClick(dataPoint)}
-                            onMouseEnter={() => handleMarkerHover(dataPoint)}
-                            onMouseLeave={handleMarkerLeave}
-                            data-tip={dataPoint.location}
-                        >
-                            {st.dataPoint === dataPoint || hoveredMarker === dataPoint ? <circle cx={0} cy={0} r={8} fill="pink" /> : ""}
-                            <circle r={4} fill="#F53" />
-                        </Marker>
-                    ))}
-                </ZoomableGroup>
-            </ComposableMap>
-            <div style={{ color: "red", textAlign: 'center', marginTop: '83px' }}>Hover on Map and scroll to zoom <FontAwesomeIcon icon={faSearchPlus} /></div>
+  useEffect(() => {
+    const mymap = L.map("map").setView([0, 0], 1, { lang: "en" }); // Set the lang option here
+
+    // Use Google Streets tile layer instead of OpenStreetMap
+    const googleStreets = L.tileLayer(
+      "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
+      {
+        maxZoom: 20,
+        subdomains: ["mt0", "mt1", "mt2", "mt3"],
+      }
+    ).addTo(mymap);
+
+    // Create a custom FontAwesome icon for the markers
+    const markerIcon = L.divIcon({
+      className: "custom-marker-icon", // Custom CSS class to style the marker icon
+      html: renderToString(
+        // Use ReactDOMServer.renderToString to convert the FontAwesome icon to HTML
+        <div style={{ fontSize: "24px", color: "red" }}>
+          <FontAwesomeIcon icon={faMapMarker} />
         </div>
+      ),
+      iconSize: [30, 30],
+      popupAnchor: [-5, -2], // Icon size (width, height) in pixels
+    });
 
-    );
+    // Add markers and popups for each location
+    locations.forEach((location) => {
+      const marker = L.marker([location.latitude, location.longitude], {
+        icon: markerIcon, // Use the custom FontAwesome icon as the marker icon
+      }).addTo(mymap);
+
+      // Custom popup content to show the location on hover
+      const popupContent = `<b>${location.location}</b>`;
+      marker.bindPopup(popupContent);
+
+      marker.on("click", () => {
+        handleMarkerClick(location); // Pass the selected marker data to the parent component
+      });
+
+      marker.on("mouseover", () => {
+        setHoveredMarker(location);
+        marker.openPopup();
+      });
+
+      marker.on("mouseout", () => {
+        setHoveredMarker(null);
+        marker.closePopup();
+      });
+    });
+
+    return () => {
+      mymap.remove();
+    };
+  }, [locations, handleMarkerClick]);
+
+  return <div id="map" style={{ height: "500px" }} />;
 }
 
-export default Map;
+export default MouseoverMap;
